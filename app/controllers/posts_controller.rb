c83684a6005b4b40d89d571061ece7082fb5686c
gitmimigit <mimi0008forg@gmail.com>
2022-05-30 16:37:59 +0900

    blog投稿フォームの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def new
     @category = Category.new
+    # @categories = Category.all.order("created_at DESC")
+    @blog = Blog.new
   end
 
   private
