808feaa40ccbbb9270f10ed8d6545cd0dcb01e82
gitmimigit <mimi0008forg@gmail.com>
2022-05-31 12:15:09 +0900

    blog詳細ページの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,10 @@ class BlogsController < ApplicationController
     Blog.create(blog_params)
   end
 
+  def show
+    @blog = Blog.find(params[:id])
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:blog_title, :blog_text).merge(user_id: current_user.id, category_id: params[:category_id])
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
   def index
-    @blog = Brog.includes(:user, :category).order("created_at DESC")
+    @blog = Blog.includes(:user, :category).order("created_at DESC")
   end
 end
