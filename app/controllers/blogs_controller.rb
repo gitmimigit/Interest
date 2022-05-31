9d386862c5fe23cbc81c17fefa5ef223131b03d2
gitmimigit <mimi0008forg@gmail.com>
2022-05-31 13:27:28 +0900

    blog編集機能の実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,14 +1,30 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :show,:update]
+
   def create
     Blog.create(blog_params)
   end
 
   def show
-    @blog = Blog.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @blog.update(blog_params)
+    @user = User.find(params[:id])
+  end
+
+  def destroy
   end
 
   private
   def blog_params
     params.require(:blog).permit(:blog_title, :blog_text).merge(user_id: current_user.id, category_id: params[:category_id])
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
