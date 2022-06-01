5411e8807710ad928cc94a256a64bbd99a6ac5d2
gitmimigit <mimi0008forg@gmail.com>
2022-06-01 15:56:29 +0900

    ビューのblog表示に関して、エラーがあったため修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @blog = @user.blogs.last
+    @blogs = @user.blogs.order(updated_at: :desc).limit(1)
+    #@blog =Blog.find(4)
   end
 end
