b00a94beadc5f340cb450886494a4f3a0ef81841
gitmimigit <mimi0008forg@gmail.com>
2022-06-01 17:20:08 +0900

    画像表示の実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -34,7 +34,7 @@ class BlogsController < ApplicationController
 
   private
   def blog_params
-    params.require(:blog).permit(:blog_title, :blog_text).merge(user_id: current_user.id, category_id: params[:category_id])
+    params.require(:blog).permit(:blog_title, :blog_text, :image).merge(user_id: current_user.id, category_id: params[:category_id])
   end
 
   def set_blog
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,5 @@ class Blog < ApplicationRecord
 
   validates :blog_title, presence: true
   validates :blog_text,  presence: true
+  has_one_attached :image
 end
