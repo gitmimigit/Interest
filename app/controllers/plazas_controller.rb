d3a31574dcbb34b9a0c867552e0f6140c04cbe55
gitmimigit <mimi0008forg@gmail.com>
2022-06-03 15:58:42 +0900

    favoriteに関する各種ビューの記述

--- app/controllers/plazas_controller.rb
+++ app/controllers/plazas_controller.rb
@@ -1,6 +1,7 @@
 class PlazasController < ApplicationController
   def index
     @blogs =Blog.order(updated_at: :desc).limit(3)
+    @favorites =Favorite.order(updated_at: :desc).limit(3)
     #@blog =Blog.find(4)
   end
 end
