64f3f2a6f07365cecebc1160a4f7124a06570db4
gitmimigit <mimi0008forg@gmail.com>
2022-06-03 14:43:37 +0900

    favoriteモデルのアソシエーションとバリデーションの記述

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :blogs
+  has_many :favorites
 
   validates :category_name, presence: true
   validates :category_name, uniqueness: { scope: :user_id }
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :categories
   has_many :blogs
+  has_many :favorites
 
   validates :user_name, presence: true, length: { maximum: 8 }
 end
