db0471136fa55e3f215d19a394e7d8ffe5521e2e
gitmimigit <mimi0008forg@gmail.com>
2022-06-06 19:44:27 +0900

    newページの実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,8 @@
 class MemosController < ApplicationController
+  before_action :set_favorite, only: [:new, :edit, :show, :update, :destroy]
+  
   def new
+    @memo = Memo.new
   end
 
   def create
@@ -12,5 +15,11 @@ class MemosController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_favorite
+    @favorite = Favorite.find(params[:favorite_id])
   end
 end
