808535537b5911a44371a8b8cb52547eea3dd7d5
gitmimigit <mimi0008forg@gmail.com>
2022-06-06 19:56:51 +0900

    createアクションの実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,11 +1,15 @@
 class MemosController < ApplicationController
-  before_action :set_favorite, only: [:new, :edit, :show, :update, :destroy]
+  before_action :set_favorite, only: [:new, :create, :edit, :show, :update, :destroy]
   
   def new
     @memo = Memo.new
   end
 
   def create
+    @memo = Memo.create(memo_params)
+    unless @memo.save
+      render :new
+    end
   end
 
   def edit
@@ -21,5 +25,9 @@ class MemosController < ApplicationController
 
   def set_favorite
     @favorite = Favorite.find(params[:favorite_id])
+  end
+
+  def memo_params
+    params.require(:memo).permit(:memo_title, :memo_text, :memo_image).merge(user_id: current_user.id, favorite_id: params[:favorite_id])
   end
 end
