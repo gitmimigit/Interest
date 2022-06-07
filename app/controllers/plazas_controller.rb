b1a03aeefecfd6fb5958f9bf801083906431c524
gitmimigit <mimi0008forg@gmail.com>
2022-06-07 20:34:35 +0900

    chartの表示

--- app/controllers/plazas_controller.rb
+++ app/controllers/plazas_controller.rb
@@ -3,5 +3,7 @@ class PlazasController < ApplicationController
     @blogs =Blog.order(updated_at: :desc).limit(3)
     @favorites =Favorite.order(updated_at: :desc).limit(3)
     #@blog =Blog.find(4)
+    @blogs_category = Blog.joins(:category).group("category_name")
+    @favorite_category = Favorite.joins(:category).group("category_name")
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,9 @@ class UsersController < ApplicationController
     @favorite = @user.favorites.order(updated_at: :desc).limit(1)
     #@blog = Blog.find(params[:id])
     #@favorite = Favorite.find(params[:id])
+    @blog_categories = @user.categories.joins(:blogs).group(:category_name).count
+    @favorite_categories = @user.categories.joins(:favorites).group(:category_name).count
+    @memo_categories = @user.categories.joins(favorites: :memos).group(:category_name).count
+    @categories = @blog_categories.merge(@favorite_categories, @memo_categories) 
   end
 end
